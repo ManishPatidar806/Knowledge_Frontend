c12602431f654878ce3bf444818bd127d6183540
ManishPatidar <manishpatidar1180@gmail.com>
2024-11-23 23:50:11 +0530

    first commit

---
+++ src/Page/Home/Navbar.jsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const menu = [
+    {
+      name: "Quizs",
+      path: "/home",
+    },
+    {
+      name: "Notes",
+      path: "/notes",
+    },
+    {
+      name: "Tutorial",
+      path: "/tutorial",
+    },
+    {
+      name: "Chatbot",
+      path: "/chatbot",
+    },
+    {
+      name: "Code Editor",
+      path: "/editer",
+    },
+
+    {
+      name: "Profile",
+      path: "/profile",
+    },
+  ];
+
+  const navigate = useNavigate();
+
+  function execuiton(path) {
+    navigate(path);
+  
+  }
+
+  return ( 
+    <div className=" flex justify-center ">
+      <div className=" list-none md:w-[50%] rounded-full  bg-white" >
+        <div className="m-3 sm:flex justify-evenly  ">
+          {menu.map((item, i) => (
+            <Button
+              variant="link"
+              className="no-underline text-black normal-case bg-transparent"
+              key={i}
+              onClick={() => execuiton(item.path)}
+            >
+              {item.name}
+            </Button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
